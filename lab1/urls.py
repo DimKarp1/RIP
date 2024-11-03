aefb3ac7aa179809fc45f99441f89e34f180ae8d
Дима К <karpenko-kda@yandex.ru>
2024-11-04 02:22:39 +0300

    lab1_fin

---
+++ lab1/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+from lab1_app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.GetMain, name='main'),
+    path('component/<int:id>/', views.GetComponent, name='component_url'),
+    path('assembly/', views.GetAssembly, name="assembly")
+]
